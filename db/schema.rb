7c1a4a2c5be92c3c562330be29568650c9f96f19
Sean Perryman <se@nperryman.com>
2015-02-22 10:42:46 -0800

    Added in the admin field and a dropdown for the application layout with admin functions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222161151) do
+ActiveRecord::Schema.define(version: 20150222181320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 20150222161151) do
     t.string   "password_salt"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.boolean  "admin"
   end
 
   create_table "worlds", force: :cascade do |t|
