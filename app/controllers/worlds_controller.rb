16fdee00e8441d643e59f16ca9bc73df39c92e34
Sean Perryman <se@nperryman.com>
2015-04-03 22:55:47 -0700

    Worked on Worlds controller and views

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -3,5 +3,59 @@ class WorldsController < ApplicationController
   	@worlds = World.all
   end
 
+  def show
+  	@world = World.find(params[:id])
+  end
 
+  def new
+    @world = World.new
+  end
+
+  def edit
+  end
+
+  def create
+    @world = World.new(world_params)
+
+    respond_to do |format|
+      if @world.save
+        format.html { redirect_to @world, notice: 'World was successfully created.' }
+        format.json { render :show, status: :created, location: @world }
+      else
+        format.html { render :new }
+        format.json { render json: @world.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @world.update(world_params)
+        format.html { redirect_to @world, notice: 'World was successfully updated.' }
+        format.json { render :show, status: :ok, location: @world }
+      else
+        format.html { render :edit }
+        format.json { render json: @world.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @world.destroy
+    respond_to do |format|
+      format.html { redirect_to worlds_url, notice: 'World was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_world
+      @world = World.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def world_params
+      params.require(:world).permit(:name)
+    end
 end
